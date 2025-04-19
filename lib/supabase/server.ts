620fb46c964de29a2d509698fe81c96ff36431cb
Tharun <tharunraman10@gmail.com>
2025-04-19 14:04:59 +0530

    supabase api fix

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,30 +1,15 @@
-import { createServerClient } from "@supabase/ssr"
-import { cookies } from "next/headers"
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import { cookies } from 'next/headers';
 
-export function createServerSupabaseClient() {
-  const cookieStore = cookies()
+export const createServerSupabaseClient = () => {
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+  const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-  return createServerClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.SUPABASE_ANON_KEY!, {
-    cookies: {
-      get(name: string) {
-        return cookieStore.get(name)?.value
-      },
-      set(name: string, value: string, options: { path: string; maxAge: number; domain?: string }) {
-        try {
-          cookieStore.set({ name, value, ...options })
-        } catch (error) {
-          // This can happen when attempting to set cookies in a middleware
-          // We can safely ignore this error
-        }
-      },
-      remove(name: string, options: { path: string; domain?: string }) {
-        try {
-          cookieStore.set({ name, value: "", ...options, maxAge: 0 })
-        } catch (error) {
-          // This can happen when attempting to delete cookies in a middleware
-          // We can safely ignore this error
-        }
-      },
-    },
-  })
-}
+  if (!supabaseUrl || !supabaseKey) {
+    throw new Error("Your project's URL and Key are required to create a Supabase client!");
+  }
+
+  return createServerComponentClient({
+    cookies,
+  });
+};
